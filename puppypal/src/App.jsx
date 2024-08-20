eb4f551d0e284e58fc0f612ee4919296cd58fa68
Noah Bonham <nowh717@gmail.com>
2024-08-20 00:53:11 -0700

    new changes

---
+++ puppypal/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+import { puppyList } from "./data.js"
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
+  
+  return (
+    <>
+      <div>
+        {
+          puppies.map((puppy) => {
+            return <p onClick={ ()=> {
+            setFeatPupId(puppy.id)
+            } } key={puppy.id}>{puppy.name}</p>
+          })
+        }
+
+        { featPupId && (
+          <div className='div1'>
+
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default App
